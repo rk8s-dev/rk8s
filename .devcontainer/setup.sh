71b786ec2fcd595e8d9961905b6d63ace24c3483
Quanyi Ma <eli@patch.sh>
2025-02-05 17:25:02 +0800

    删除 zsh 安装，在 root 目录下载 buck2
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -36,18 +36,9 @@ rustup component add clippy
 cargo install cargo-expand
 cargo install cargo-edit
 
-## Setup and install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp -R /root/.oh-my-zsh /home/$USERNAME
-cp /root/.zshrc /home/$USERNAME
-sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
-
 ## Install Buck2
-wget https://github.com/facebook/buck2/releases/download/2025-02-01/buck2-x86_64-unknown-linux-musl.zst /home/$USERNAME/buck2-x86_64-unknown-linux-musl.zst
-zstd -d /home/$USERNAME/buck2-x86_64-unknown-linux-musl.zst
-mkdir -p /home/$USERNAME/.buck/bin
-mv /home/$USERNAME/buck2-x86_64-unknown-linux-musl /home/$USERNAME/.buck/bin/buck2
-chmod +x /home/$USERNAME/.buck/bin/buck2
-chown -R $USER_UID:$USER_GID /home/$USERNAME/.buck
-export PATH="/home/$USERNAME/.buck2/bin:$PATH"
+wget https://github.com/facebook/buck2/releases/download/2025-02-01/buck2-x86_64-unknown-linux-musl.zst /root/buck2-x86_64-unknown-linux-musl.zst
+zstd -d /root/buck2-x86_64-unknown-linux-musl.zst
+mv /root/buck2-x86_64-unknown-linux-musl /root/buck2
+chmod +x /root/buck2
+mv /root/buck2 /usr/local/bin/buck2
