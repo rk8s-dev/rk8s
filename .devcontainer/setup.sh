b4dd7eaa0ceefc0fe390b30eb7e094d6d3c7ee41
Quanyi Ma <eli@patch.sh>
2025-03-03 17:55:50 +0800

    将 project 整体设置为 workspace ， rklet 等项目中使用其它模块
    Signed-off-by: Quanyi Ma <eli@patch.sh>

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,36 +1,30 @@
 ## update and install some things we should probably have
 apt-get update
+apt-get upgrade -y
 apt-get install -y \
+  apt-utils \
   curl \
   git \
   gnupg2 \
   jq \
-  sudo \
-  zsh \
   build-essential \
   openssl \
   libssl-dev \
-  fuse3 \
-  libfuse3-dev \
   pkg-config \
   cmake \
-  clang \
   wget \
   file \
   ca-certificates \
-  zstd
+  zstd \
+  clang \
+  lld
 
 ## Install rustup and common components
 curl https://sh.rustup.rs -sSf | sh -s -- -y
-rustup install default
-rustup component add rustfmt
-rustup component add clippy
-
-cargo install cargo-expand
-cargo install cargo-edit
+source /root/.cargo/env
 
 ## Install Buck2
-wget https://github.com/facebook/buck2/releases/download/2025-02-01/buck2-x86_64-unknown-linux-musl.zst
+wget https://github.com/facebook/buck2/releases/download/2025-03-01/buck2-aarch64-apple-darwin.zst
 zstd -d /home/buck2-x86_64-unknown-linux-musl.zst
 mv /home/buck2-x86_64-unknown-linux-musl /home/buck2
 chmod +x /home/buck2
