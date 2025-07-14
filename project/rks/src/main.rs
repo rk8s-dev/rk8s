fb9446764dfd5b45478522106a58c5dec8298cf5
ichAB <820097351@qq.com>
2025-07-14 17:55:10 +0800

    lock the version of aws-ls-sys 0.29.0
    Signed-off-by: ichAB <820097351@qq.com>

--- project/rks/src/main.rs
+++ project/rks/src/main.rs
@@ -9,8 +9,12 @@ use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config_path = env::var("RKS_CONFIG_PATH")
-        .unwrap_or_else(|_| format!("{}/tests/config.yaml", std::env::var("CARGO_MANIFEST_DIR").unwrap()));
+    let config_path = env::var("RKS_CONFIG_PATH").unwrap_or_else(|_| {
+        format!(
+            "{}/tests/config.yaml",
+            std::env::var("CARGO_MANIFEST_DIR").unwrap()
+        )
+    });
     let cfg: Config = load_config(&config_path)?;
     let endpoints: Vec<&str> = cfg.xline_endpoints.iter().map(|s| s.as_str()).collect();
     let xline_store = Arc::new(XlineStore::new(&endpoints).await?);
