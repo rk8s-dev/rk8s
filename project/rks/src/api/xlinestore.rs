33da88b2b45ad109de6cbc36270ed924ab4a527d
tangzhouxuan-nju <100063672+ichAB@users.noreply.github.com>
2025-08-13 16:22:30 +0800

    definition of node (#111)
    * definition of node
    
    Signed-off-by: ichAB <820097351@qq.com>
    
    * modify according to copilot
    
    Signed-off-by: ichAB <820097351@qq.com>
    
    * fix rustfmt error
    
    Signed-off-by: ichAB <820097351@qq.com>
    
    ---------
    
    Signed-off-by: ichAB <820097351@qq.com>

--- project/rks/src/api/xlinestore.rs
+++ project/rks/src/api/xlinestore.rs
@@ -1,6 +1,6 @@
+use crate::protocol::Node;
 use anyhow::Result;
 use etcd_client::{Client, GetOptions, PutOptions};
-use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::sync::Arc;
 use tokio::sync::RwLock;
@@ -9,12 +9,6 @@ use tokio::sync::RwLock;
 #[derive(Clone)]
 pub struct XlineStore {
     client: Arc<RwLock<Client>>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct NodeInfo {
-    pub ip: String,
-    pub status: String,
 }
 
 impl XlineStore {
@@ -54,30 +48,30 @@ impl XlineStore {
             .collect())
     }
 
-    pub async fn insert_node_info(&self, node_name: &str, ip: &str, status: &str) -> Result<()> {
+    pub async fn insert_node_yaml(&self, node_name: &str, node_yaml: &str) -> Result<()> {
         let key = format!("/registry/nodes/{node_name}");
-        let value = serde_yaml::to_string(&NodeInfo {
-            ip: ip.to_string(),
-            status: status.to_string(),
-        })?;
         let mut client = self.client.write().await;
-        client.put(key, value, Some(PutOptions::new())).await?;
+        client.put(key, node_yaml, Some(PutOptions::new())).await?;
         Ok(())
     }
 
-    // pub async fn get_node_info(&self, node_name: &str) -> Result<Option<NodeInfo>> {
-    //     let key = format!("/registry/nodes/{}", node_name);
+    // pub async fn get_node_yaml(&self, node_name: &str) -> Result<Option<String>> {
+    //     let key = format!("/registry/nodes/{node_name}");
     //     let mut client = self.client.write().await;
     //     let resp = client.get(key, None).await?;
-    //     if let Some(kv) = resp.kvs().first() {
-    //         let info: NodeInfo = serde_yaml::from_slice(kv.value())?;
-    //         Ok(Some(info))
+    //     Ok(resp.kvs().first().map(|kv| String::from_utf8_lossy(kv.value()).to_string()))
+    // }
+
+    // pub async fn get_node(&self, node_name: &str) -> Result<Option<Node>> {
+    //     if let Some(yaml) = self.get_node_yaml(node_name).await? {
+    //         let node: Node = serde_yaml::from_str(&yaml)?;
+    //         Ok(Some(node))
     //     } else {
     //         Ok(None)
     //     }
     // }
 
-    pub async fn list_nodes(&self) -> Result<Vec<(String, NodeInfo)>> {
+    pub async fn list_nodes(&self) -> Result<Vec<(String, Node)>> {
         let mut client = self.client.write().await;
         let resp = client
             .get("/registry/nodes/", Some(GetOptions::new().with_prefix()))
@@ -85,8 +79,8 @@ impl XlineStore {
         let mut result = Vec::new();
         for kv in resp.kvs() {
             let name = String::from_utf8_lossy(kv.key()).replace("/registry/nodes/", "");
-            let info: NodeInfo = serde_yaml::from_slice(kv.value())?;
-            result.push((name, info));
+            let node: Node = serde_yaml::from_slice(kv.value())?;
+            result.push((name, node));
         }
         Ok(result)
     }
