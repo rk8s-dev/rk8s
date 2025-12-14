b2b7f94d43856ed1cb86d28b4fd349d2167b574a
ichAB <820097351@qq.com>
2025-12-14 20:41:46 +0800

    fix gc error
    Signed-off-by: ichAB <820097351@qq.com>

--- project/rks/src/controllers/garbage_collector.rs
+++ project/rks/src/controllers/garbage_collector.rs
@@ -202,7 +202,7 @@ impl Controller for GarbageCollector {
     }
 
     fn watch_resources(&self) -> Vec<ResourceKind> {
-        vec![ResourceKind::Pod, ResourceKind::ReplicaSet]
+        vec![ResourceKind::Pod, ResourceKind::ReplicaSet,ResourceKind::Deployment]
     }
 
     async fn handle_watch_response(
