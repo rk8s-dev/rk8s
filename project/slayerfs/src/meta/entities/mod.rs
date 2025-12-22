3d37a503ae02e36363655ff0671b5aa7e1c612b6
雷家兴 <leij49456@gmail.com>
2025-12-22 23:25:49 +0800

    feat: add linkParentMeta entity for hardlink support
    Signed-off-by: 雷家兴 <leij49456@gmail.com>

--- project/slayerfs/src/meta/entities/mod.rs
+++ project/slayerfs/src/meta/entities/mod.rs
@@ -3,6 +3,7 @@ pub mod access_meta;
 pub mod content_meta;
 pub mod etcd;
 pub mod file_meta;
+pub mod link_parent_meta;
 pub mod locks_meta;
 pub mod plock_meta;
 pub mod session_meta;
@@ -11,6 +12,7 @@ pub mod slice_meta;
 pub use access_meta::{Entity as AccessMeta, Model as AccessMetaModel};
 pub use content_meta::{Entity as ContentMeta, EntryType, Model as ContentMetaModel};
 pub use file_meta::{Entity as FileMeta, Model as FileMetaModel};
+pub use link_parent_meta::Entity as LinkParentMeta;
 pub use locks_meta::Entity as LocksMeta;
 pub use plock_meta::Entity as PlockMeta;
 #[allow(unused_imports)]
