47710b04ff152bf5bf99e0e44b3801e174f1a6ce
erasernoob <erasernoobx@outlook.com>
2026-03-09 19:09:19 +0800

    fix(clippy): fix the clippy error conflicts due to version mismached, details see: https://github.com/rk8s-dev/rk8s/actions/runs/22847104888/job/66266148165

--- project/distribution/src/api/mod.rs
+++ project/distribution/src/api/mod.rs
@@ -25,7 +25,8 @@ use std::sync::Arc;
 
 pub fn create_router(state: Arc<AppState>) -> Router<()> {
     // we need to handle both /v2 and /v2/
-    let router = Router::new()
+    #[allow(unused_mut)]
+    let mut router = Router::new()
         .route("/healthz", get(healthz))
         .route("/v2/", get(probe))
         .nest("/v2", v2::create_v2_router(state.clone()))
