e376e1c3bae19b7be514658586d373423ada6d6d
jackieismpc <jackieismpc@gmail.com>
2026-03-17 15:21:07 +0800

    del old example
    Signed-off-by: jackieismpc <jackieismpc@gmail.com>

--- project/dagrs/src/utils/execstate.rs
+++ project/dagrs/src/utils/execstate.rs
@@ -12,14 +12,6 @@ pub(crate) struct ExecState {
     success: AtomicBool,
     /// Output produced by a task.
     output: Arc<Mutex<Output>>,
-    /*/// The semaphore is used to control the synchronous blocking of subsequent tasks to obtain the
-    /// execution results of this task.
-    /// When a task is successfully executed, the permits inside the semaphore will be increased to
-    /// n (n represents the number of successor tasks of this task or can also be called the output
-    /// of the node), which means that the output of the task is available, and then each successor
-    /// The task will obtain a permits synchronously (the permit will not be returned), which means
-    /// that the subsequent task has obtained the execution result of this task.
-    semaphore: Semaphore,*/
 }
 
 impl ExecState {
@@ -55,13 +47,4 @@ impl ExecState {
     pub(crate) fn exe_fail(&self) {
         self.success.store(false, Ordering::Relaxed)
     }
-
-    /*/// The semaphore is used to control the synchronous acquisition of task output results.
-    /// Under normal circumstances, first use the semaphore to obtain a permit, and then call
-    /// the `get_output` function to obtain the output. If the current task is not completed
-    /// (no output is generated), the subsequent task will be blocked until the current task
-    /// is completed and output is generated.
-    pub(crate) fn semaphore(&self) -> &Semaphore {
-        &self.semaphore
-    }*/
 }
