b96277ab699cd44637e4b39b3601e2ecd53517df
jackieismpc <jackieismpc@gmail.com>
2026-03-23 11:29:24 +0800

    dagrs-derive: harden auto_node macro expansion
    Signed-off-by: jackieismpc <jackieismpc@gmail.com>

---
+++ project/dagrs-derive/tests/ui/fail/reserved_field/src/main.rs
@@ -0,0 +1,8 @@
+use dagrs_derive::auto_node;
+
+#[auto_node]
+struct InvalidNode {
+    id: usize,
+}
+
+fn main() {}
---
+++ project/dagrs-derive/tests/ui/fail/tuple_struct/src/main.rs
@@ -0,0 +1,6 @@
+use dagrs_derive::auto_node;
+
+#[auto_node]
+struct InvalidNode(u32);
+
+fn main() {}
